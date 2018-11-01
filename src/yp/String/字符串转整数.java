b035b041c501e95005ed21748846b34a9b317694
RickYinPeng <272940172@qq.com>
2018-11-01 23:20:12 +0800

    /**  * @author RickYinPeng  * @ClassName 字符串转整数  * @Description LeetCode中第8道题  * @date 2018/11/1/21:14  */

---
+++ src/yp/String/字符串转整数.java
@@ -0,0 +1,161 @@
+package yp.String;
+
+import org.junit.Test;
+
+/**
+ * @author RickYinPeng
+ * @ClassName 字符串转整数
+ * @Description LeetCode中第8道题
+ * @date 2018/11/1/21:14
+ */
+public class 字符串转整数 {
+
+    public static void main(String[] args) {
+        String str = "  -0012a42";
+        int i = myAtoi(str);
+        System.out.println(i);
+    }
+    public static int myAtoi(String str) {
+        if(str.trim().equals("") || str==null){
+            return 0;
+        }
+
+        int i = 0;
+        char c = str.charAt(i);
+        while(c==' '){
+            i++;
+            c = str.charAt(i);
+        }
+
+        //到达非空字符串
+        if(i<str.length()){
+            //如果第一个字符不是一个有效的整数
+            if(!Character.isDigit(c) && c!='+' && c!='-'){
+                return 0;
+            }
+            //是数字或者加减号
+            if(Character.isDigit(c)){
+                String s = String.valueOf(c);
+
+                i++;
+                if(i<str.length()){
+                    c = str.charAt(i);
+                }
+
+                while(Character.isDigit(c) && i<str.length()){
+                    s += c;
+                    i++;
+                    if(i<str.length()){
+                        c = str.charAt(i);
+                    }else{
+                        break;
+                    }
+                }
+
+                Long aLong = null;
+                try {
+                    aLong = Long.valueOf(s);
+                } catch (NumberFormatException e) {
+                    return Integer.MAX_VALUE;
+                }
+
+                if (aLong > Integer.MAX_VALUE) {
+                    return Integer.MAX_VALUE;
+                }
+                if (aLong < Integer.MIN_VALUE) {
+                    return Integer.MIN_VALUE;
+                }
+                int result = aLong.intValue();
+                return result;
+            }else{
+                String s = String.valueOf(c);
+                i++;
+
+                if(i<str.length()){
+                    if(str.charAt(i)>'9' || str.charAt(i)<'0'){
+                        return 0;
+                    }
+
+                    while (str.charAt(i)=='0'){
+                        i++;
+                    }
+
+                    if(i<str.length()){
+                        String x = str.substring(i);
+
+
+
+                        x = x.replaceAll("[^\\d]"," ");
+
+                        int index = 0;
+                        int length = 0;
+                        String[] split = x.split(" ");
+                        int j;
+                        for (j= 0;j<split.length;j++){
+                            //System.out.println(split[j]);
+                            if(split[j].length()>length){
+                                length = split[j].length();
+                                index = j;
+                            }
+                        }
+
+                        s +=split[index];
+
+                        Long aLong = null;
+                        try {
+                            aLong = Long.valueOf(s);
+                        } catch (NumberFormatException e) {
+                            return 0;
+                        }
+
+                        if (aLong > Integer.MAX_VALUE) {
+                            return Integer.MAX_VALUE;
+                        }
+                        if (aLong < Integer.MIN_VALUE) {
+                            return Integer.MIN_VALUE;
+                        }
+                        int result = aLong.intValue();
+                        return result;
+                    }else{
+                        return 0;
+                    }
+
+                }else{
+                    return 0;
+                }
+
+
+            }
+        }
+        return 0;
+    }
+
+    /*public static int myAtoi(String str) {
+        String s;
+        s = str.replaceAll(" ", "");
+        s = s.replaceAll("[^\\d+-]", "");
+
+        Long aLong = Long.valueOf(s);
+        if (aLong > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if (aLong < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        int i = aLong.intValue();
+
+        return i;
+    }*/
+
+    @Test
+    public void test(){
+        String s = "  +ad123123";
+        s = s.replaceAll(" ","");
+        s = s.replaceAll("[^\\d+-]","");
+        System.out.println("s:"+s);
+
+
+        String[] ss = {"adssa","dasd","dasd","dasda"};
+    }
+}
