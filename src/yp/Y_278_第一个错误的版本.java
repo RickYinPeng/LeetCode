b4cfc9cfaaa21531f22c141099d87304860bb800
RickYinPeng <272940172@qq.com>
2019-03-29 01:17:29 +0800

    Y_278_第一个错误的版本

--- src/yp/Y_278_第一个错误的版本.java
+++ src/yp/Y_278_第一个错误的版本.java
@@ -7,7 +7,33 @@ package yp;
  * @date 2019/3/28/23:41
  */
 public class Y_278_第一个错误的版本 {
+    public static void main(String[] args) {
 
+    }
 
+    public int firstBadVersion(int n) {
+
+        int low = 1;
+        int high = n;
+
+        while (low<=high){
+            int mid = low+((high-low)>>1);
+
+            if(!isBadVersion(mid)){
+                low = mid+1;
+            }else {
+                if(isBadVersion(mid-1)){
+                    high = mid-1;
+                }else {
+                    return mid;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private boolean isBadVersion(int mid) {
+        return true;
+    }
 
 }
