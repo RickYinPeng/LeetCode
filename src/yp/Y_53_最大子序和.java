978812d884606fadccca46147952d728513d0f5b
RickYinPeng <272940172@qq.com>
2019-04-01 01:49:39 +0800

    Y_53_最大子序和

--- src/yp/Y_53_最大子序和.java
+++ src/yp/Y_53_最大子序和.java
@@ -8,12 +8,22 @@ package yp;
  */
 public class Y_53_最大子序和 {
     public static void main(String[] args) {
-
+        int[] ar = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        //int[] ar = new int[]{-1,-2,-3,-4,-5,-6,-7,-8,-9};
+        int i = maxSubArray(ar);
+        System.out.println(i);
     }
-    public int maxSubArray(int[] nums) {
-        
-
-
-        return 0;
+    public static int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if(sum>0){
+                sum += num;
+            }else {
+                sum = num;
+            }
+            max = Math.max(sum, max);
+        }
+        return max;
     }
 }
